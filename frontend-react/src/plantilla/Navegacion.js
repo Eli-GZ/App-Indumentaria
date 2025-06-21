d3243c313dd77825f520cd37fd594fe6f87cdb0b
Elias <elias.d.gonzalez25@gmail.com>
2025-06-20 21:42:37 -0300

    update

--- frontend-react/src/plantilla/Navegacion.js
+++ frontend-react/src/plantilla/Navegacion.js
@@ -10,13 +10,13 @@ export default function Navegacion({ logueado, setLogueado }) {
     navigate('/');
   };
   return (       
-       <div className="d-flex">
+       <div className="d-flex justify-content-center">
       {/* Sidebar lateral fijo */}
       <div
         className="bg-dark text-white vh-100 p-3"
         style={{ width: '250px', position: 'fixed', left: 0, top: 0 }}
       >
-        <h5 className="text-white mb-4">Control de stock - Indumentaria Suzy</h5>
+        <h5 className="text-white mb-4">Control de stock Indumentaria Suzy</h5>
         <ul className="nav flex-column fs-5">
           {logueado && (
             <>
